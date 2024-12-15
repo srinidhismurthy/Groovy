7a2bcee875ed712eee62c9d8e0cba05cb2533c49
srinidhismurthy <srinidhismurthy@gmail.com>
2024-12-15 19:57:56 +0530

    Update test_google_search.py
    reverted changes to driver import

--- test_google_search.py
+++ test_google_search.py
@@ -1,9 +1,9 @@
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 def test_google_search():
-    driver = ChromeDriverManager().install()
+    driver = webdriver.Chrome()
     driver.get("https://www.google.com")
     
     search_box = driver.find_element(By.NAME, "q")
